ae872439b88e4daa5401034b422a70b68aa7a4c9
Julian Viereck <julian@kyberlabs.ai>
2025-05-11 10:04:12 -0400

    Bench: Trying to render line via gl directly. Not much sizes

--- js/bench.js
+++ js/bench.js
@@ -1,63 +1,83 @@
 let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext("2d");
 
-const offCanvas = new OffscreenCanvas(canvas.width, canvas.height);
-const offCtx = offCanvas.getContext('2d');
+drawer = new GLDrawer(canvas);
+
+const N = 5;
+let linesCenter = new Float32Array(2 * N);
+
+let x = -1;
+let dx = 2 / N;
+
+for (let i = 0; i < N; i++) {
+    linesCenter[2 * i] = x;
+    linesCenter[2 * i + 1] = Math.sin(2 * Math.PI * x);
+    x += dx;
+}
 
 
-p = new Path2D();
-
-w = 800 * 4;
-const N = w;
-dt = 2 * Math.PI / N;
-dx = w / N;
-
-let x = 0;
-let t = 0
-
-let pxy = new Float32Array(2 * N);
-
-function fillPath(p, pxy) {
-    for (let i = 0; i < 2*N; i += 2) {
-        p.lineTo(pxy[i], pxy[i + 1]);
+class LineRenderer {
+    constructor() {
+        this.dataVersion = 0;
+        this.gpuLineCenter = new GPUData(this, linesCenter);
     }
 }
 
-function stroke(ctx, p) {
-    ctx.lineWidth = 4;
-    ctx.stroke(p);
-}
 
-function fillData(pxy) {
-    for (let i = 0; i < N; i++) {
-        pxy[2 * i] = Math.round(x);
-        if (i % 2 == 0) {
-            pxy[2 * i + 1] = 0;
-        } else {
-            pxy[2 * i + 1] = 300 * 4
+
+class GLLineStripDrawer {
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.setViewport(-1, 1, -1, 1)
+        this.createGLProgram();
+    }
+
+    createGLProgram() {
+        let gl = this.ctx.gl;
+        const program = this.program = gl.createProgram();
+        gl.attachShader(program, this.ctx.vertexShader);
+        gl.attachShader(program, this.ctx.fragmentShader);
+        gl.linkProgram(program);
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            throw gl.getProgramInfoLog(program);
         }
-        // pxy[2 * i + 1] = 150 * 2 + 2 * 150 * Math.sin(t);
-        x += dx;
-        t += dt;
+        gl.useProgram(program);
+
+        this.colorUniformLocation = gl.getUniformLocation(program, "u_color");
+        this.zUniformLocation = gl.getUniformLocation(program, "u_z");
+    }
+
+    bindData(attributeName, gpuData, size) {
+        const gl = this.ctx.gl;
+        gpuData.bind(gl);
+
+        const attribute = gl.getAttribLocation(this.program, attributeName);
+        gl.enableVertexAttribArray(attribute);
+        gl.vertexAttribPointer(attribute, size, gl.FLOAT, false, 0, 0);
+    }
+
+    clear() {
+        let gl = this.ctx.gl;
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+
+    drawLineChunk(lineChunk, style) {
+        let gl = this.ctx.gl;
+        let canvas = this.ctx.canvas;
+        let program = this.program;
+        gl.useProgram(program);
+
+        this.bindData('lineCenter', lineChunk.gpuLineCenter, 2, );
+
+        gl.uniform4fv(this.colorUniformLocation, [style.r, style.g, style.b, 1.])
+        gl.uniform1f(this.zUniformLocation, style.z);
+
+        gl.drawArrays(gl.LINE_STRIP , lineChunk.from, lineChunk.to - lineChunk.from);
+    }
+
+    updateSize(width, height) {
+        this.setViewport.apply(this, this.viewport);
     }
 }
 
-function paint() {
-    p.moveTo(0, 150 * 4);
-
-    fillData(pxy);
-    fillPath(p, pxy);
-    stroke(offCtx, p);
-
-    let imData = offCtx.getImageData(0, 0, 10, 10);
-    ctx.putImageData(imData, 0, 0);
-}
-
-r = 200
-setTimeout(() => {
-    paint();
-}, r)
-
-// setTimeout(() => {
-//     paint();
-// }, r + 1)
+let gl = drawer.gl;
+console.log(gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE));
